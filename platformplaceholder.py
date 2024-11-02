2e1483437c9f87699138d45aaaabc53ee180fee9
rblazer03 <rblazer@zagmail.gonzaga.edu>
2024-11-02 11:19:49 -0700

    add files

--- platformplaceholder.py
+++ platformplaceholder.py
@@ -5,4 +5,6 @@ main.title('Hackathon Platformer')
 gameSpace = tk.Canvas(main, width=1500, height=900)
 gameSpace.pack()
 
+gameSpace.create_rectangle((100, 200), (900, 300), fill='green')
+
 main.mainloop()

ff696e84429cda42b453b6a2f4771bc09f3196b0
rblazer03 <rblazer@zagmail.gonzaga.edu>
2024-11-02 11:06:10 -0700

    canvas space

--- platformplaceholder.py
+++ platformplaceholder.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import tkinter as tk
+main = tk.Tk()
+
+main.title('Hackathon Platformer')
+gameSpace = tk.Canvas(main, width=1500, height=900)
+gameSpace.pack()
+
+main.mainloop()
